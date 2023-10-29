d775cec7fb6df2642ac3d4a1c8b696b938107719
blfuentes <blfuentes@mundo-r.com>
2023-10-29 21:01:24 +0100

    fixsed app tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title "pet-health-manager"`, () => {
+  it(`should have as title "Pet health manager"`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("pet-health-manager");
+    expect(app.title).toEqual("Pet health manager");
   });
 
   it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector(".content span")?.textContent).toContain("pet-health-manager app is running!");
+    expect(compiled.querySelector("h1")?.textContent).toContain("Pet health manager");
   });
 });
