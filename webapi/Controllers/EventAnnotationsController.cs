5136067452d831b55cd4af775e711afa89eaa8a8
blfuentes <epiphonetodie@gmail.com>
2024-09-20 08:08:30 +0200

    updated nuget and node packages

--- webapi/Controllers/EventAnnotationsController.cs
+++ webapi/Controllers/EventAnnotationsController.cs
@@ -11,8 +11,8 @@ public class EventAnnotationsController(IMediator mediator) : ControllerBase
     [HttpGet("{petId}")]
     public async Task<IEnumerable<GetEventAnnotationsByPetResponse>> GetEventAnnotationsByPetId(int petId)
     {
-        var weights = await mediator.Send(new GetEventAnnotationsByPetQuery(petId));
-        return weights;
+        var annotations = await mediator.Send(new GetEventAnnotationsByPetQuery(petId));
+        return annotations;
     }
 }
 
